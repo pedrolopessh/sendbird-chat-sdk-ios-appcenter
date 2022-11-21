829d180ec48ae127013eaf0bfc389a78760ea759
Pedro Lopes <p.lopes@swordhealth.com>
2022-11-21 08:29:00 +0000

    Add local sendbird lib

--- Package.swift
+++ Package.swift
@@ -15,8 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SendbirdChatSDK",
-            url: "https://github.com/sendbird/sendbird-chat-sdk-ios/releases/download/v4.1.6/SendbirdChatSDK.xcframework.zip",
-            checksum: "3a038c8739308151c1b405e7fb8f12592a59aedab2b884d421bba2343543adfc"
+			path: "./SendbirdChatSDK.xcframework"
         ),
     ]
 )
